18804c3cce85dd0e6f68e99c97300509683f7ed5
Guillaumelsn <guillaumelesenne@gmail.com>
2022-06-08 11:03:50 +0200

    xx
    xx

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BlogController {
 @RequestMapping("/")
     public String listPosts(ModelMap map) {
         map.put("title", "Blog Post 1");
+        List<Post> posts = new ArrayList<Post>(postRepository.getAllPosts());
+        map.put("posts", posts);
         return "home";
     }
 
+    private PostRepository postRepository = new PostRepository();
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 }
